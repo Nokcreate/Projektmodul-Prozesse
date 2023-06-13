72af879491d232b2287f3fc06b256e68cd75b41f
Nokcreate <114598730+Nokcreate@users.noreply.github.com>
2023-06-13 17:34:27 +0200

    matratze mit schoenerer on click animation

--- Assets/Clinic - Hospital room/scripts/MatressExpander.cs
+++ Assets/Clinic - Hospital room/scripts/MatressExpander.cs
@@ -6,13 +6,16 @@ public class MatressExpander : MonoBehaviour
     public GameObject[] layers = new GameObject[10];
     public float expansionDistance = 0.2f;
     public OVRInput.Button triggerButton = OVRInput.Button.PrimaryIndexTrigger;
+    public float moveSpeed = 1.0f; // Geschwindigkeit, mit der sich die Schichten bewegen
     private bool isExpanded = false;
+    private Vector3[] originalPositions; // speichert die ursprünglichen Positionen der Schichten
 
     private void Start()
     {
-        // Sie müssen diese Zeilen durch den Namen Ihrer GameObjects ersetzen.
-        layers[0] = GameObject.Find("AI_Sensor");
-        layers[1] = GameObject.Find("Boden_oben_AI_Sensor");
+        originalPositions = new Vector3[layers.Length];
+        // Ersetzen Sie diese Zeilen durch den Namen Ihrer GameObjects.
+        layers[0] = GameObject.Find("Boden_oben_AI_Sensor");
+        layers[1] = GameObject.Find("AI_Sensor");
         layers[2] = GameObject.Find("Boden_unten_AI_Sensor");
         layers[3] = GameObject.Find("BodenLuftKissen");
         layers[4] = GameObject.Find("BodenVisko");
@@ -20,6 +23,12 @@ public class MatressExpander : MonoBehaviour
         layers[6] = GameObject.Find("Luftkissen");
         layers[7] = GameObject.Find("ViskoCube");
         layers[8] = GameObject.Find("WärmeKälteLayer");
+
+        // Speichert die ursprünglichen Positionen der Schichten
+        for (int i = 0; i < layers.Length; i++)
+        {
+            originalPositions[i] = layers[i].transform.position;
+        }
     }
 
     private void Update()
@@ -43,10 +52,8 @@ public class MatressExpander : MonoBehaviour
     {
         for (int i = 0; i < layers.Length; i++)
         {
-            Vector3 newPosition = layers[i].transform.position;
-            newPosition.y += i * expansionDistance;
-            layers[i].transform.position = newPosition;
-            yield return new WaitForSeconds(0.2f);
+            Vector3 targetPosition = originalPositions[i] + new Vector3(0, i * expansionDistance, 0);
+            yield return StartCoroutine(MoveToTarget(layers[i].transform, targetPosition));
         }
     }
 
@@ -54,10 +61,19 @@ public class MatressExpander : MonoBehaviour
     {
         for (int i = layers.Length - 1; i >= 0; i--)
         {
-            Vector3 newPosition = layers[i].transform.position;
-            newPosition.y -= i * expansionDistance;
-            layers[i].transform.position = newPosition;
-            yield return new WaitForSeconds(0.2f);
+            Vector3 targetPosition = originalPositions[i];
+            yield return StartCoroutine(MoveToTarget(layers[i].transform, targetPosition));
         }
+    }
+
+    private IEnumerator MoveToTarget(Transform transform, Vector3 targetPosition)
+    {
+        while ((transform.position - targetPosition).magnitude > 0.01f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        transform.position = targetPosition; // um sicherzustellen, dass das Ziel exakt erreicht wird
     }
 }
